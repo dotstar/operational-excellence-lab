389bcd6ac8559220528a46bec9040b189659861f
Dave <cdd@gmail.com>
2019-12-20 13:04:00 -0600

    work on cleanup logic

--- cfn/CleanDeletionLambdaFunction.py
+++ cfn/CleanDeletionLambdaFunction.py
@@ -10,13 +10,6 @@ ecrClient = boto3.client('ecr')
 ecsClient = boto3.client('ecs')
 s3 = boto3.resource('s3')
 
-#  Copyright 2016 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
-#  This file is licensed to you under the AWS Customer Agreement (the "License").
-#  You may not use this file except in compliance with the License.
-#  A copy of the License is located at http://aws.amazon.com/agreement/ .
-#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
-#  See the License for the specific language governing permissions and limitations under the License.
-
 from botocore.vendored import requests
 import json
 
@@ -41,7 +34,7 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
 
     json_responseBody = json.dumps(responseBody)
 
-    print("Response body:\n" + json_responseBody)
+    print("Response body:\n" + json_responseBody,flush=True)
 
     headers = {
         'content-type': '',
@@ -56,100 +49,119 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
     except Exception as e:
         print("send(..) failed executing requests.put(..): " + str(e))
 
+def failfast(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
+    print("Goodbye world!", flush=True)
+    r = {}
+    r['Data'] = responseData
+    print(r)
+    print("responseData Type: ",type(r),flush=True)
+    send(event, context, SUCCESS, r, 'CustomResourcePhysicalID')
+    exit(0)
 
 def handler(event, context):
-   print(event)
-   CoreServiceEcrRepo = event['ResourceProperties']['CoreServiceRepo']
-   LikeServiceEcrRepo = event['ResourceProperties']['LikeServiceRepo']
-   EcsClusterName = event['ResourceProperties']['EcsClusterName']
+    try:
+        CoreServiceEcrRepo = event['ResourceProperties']['CoreServiceRepo']
+        LikeServiceEcrRepo = event['ResourceProperties']['LikeServiceRepo']
+        EcsClusterName = event['ResourceProperties']['EcsClusterName']
+        CfnStackName = event['ResourceProperties']['CfnStackName']
+        IsSecondary = event['ResourceProperties']['SecondaryRegion']
+        MythicalArtifactBucket = event['ResourceProperties']['MythicalArtifactBucket']
+        logger.info('Event: {}'.format(event))
+        logger.info('Context: {}'.format(context))
+        tempResponse = []
+        responseData = {}
+        failure = False
+        print(event,flush=True)
+        logging.info(event)
+    except Exception as e:
+        logger.warning('failure parsing parameters from event {}'.format(str(e)))
+        print('parsing inputs failed',flush=True)
+        send(event, context, FAILED, 'unknown failure', 'CustomResourcePhysicalID')
+        return()
 
-   CfnStackName = event['ResourceProperties']['CfnStackName']
-   IsSecondary = event['ResourceProperties']['SecondaryRegion']
-   if IsSecondary == 'false':
-      MythicalArtifactBucket = event['ResourceProperties']['MythicalArtifactBucket']
-   logger.debug('Event: {}'.format(event))
-   logger.debug('Context: {}'.format(context))
-   tempResponse = []
-   responseData = {}
-   failure = False
-   # Immediately respond on Delete
-   if event['RequestType'] == 'Delete':
-      try:
-         coreDeleteResponse = ecrClient.delete_repository(
-            repositoryName=CoreServiceEcrRepo,
-            force=True)
-      except Exception as e:
-         if "RepositoryNotFoundException" in str(e):
-            failure = False
-         else:
-            failure = True
-            tempResponse.append({CoreServiceEcrRepo:str(e)})
-      try:
-         likeDeleteResponse = ecrClient.delete_repository(
-             repositoryName=LikeServiceEcrRepo,
-             force=True)
-      except Exception as e:
-         if "RepositoryNotFoundException" in str(e):
-            failure = False
-         else:
-            failure = True
-            tempResponse.append({LikeServiceEcrRepo:str(e)})
-
-      if IsSecondary == 'false':
-         try:
-            bucket = s3.Bucket(MythicalArtifactBucket)
-            # Delete S3 bucket
-            for key in bucket.objects.all():
-               key.delete()
-               bucket.delete()
-         except Exception as e:
-            if "The specified bucket does not exist" in str(e):
-               failure = False
+    # Immediately respond on Delete
+    if event['RequestType'] == 'Delete':
+        try:
+            logger.info('Delete called')
+            coreDeleteResponse = ecrClient.delete_repository(
+                repositoryName=CoreServiceEcrRepo,
+                force=True)
+        except Exception as e:
+            if "RepositoryNotFoundException" in str(e):
+                failure = False
             else:
-               failure = True
-            tempResponse.append({MythicalArtifactBucket:str(e)})
-         if EcsClusterName != "testing-foo-cdd":   # Skip this code while we're running in test harness
+                failure = True
+            tempResponse.append({CoreServiceEcrRepo: str(e)})
+        try:
+            likeDeleteResponse = ecrClient.delete_repository(
+               repositoryName=LikeServiceEcrRepo,
+               force=True)
+        except Exception as e:
+            if "RepositoryNotFoundException" in str(e):
+                failure = False
+            else:
+                failure = True
+            tempResponse.append({LikeServiceEcrRepo: str(e)})
+        if IsSecondary == 'false':
             try:
-               # List ECS Services and delete services in a specific cluster
-               ecsServicesToDelete = ecsClient.list_services(cluster=EcsClusterName)
-               if ecsServicesToDelete['serviceArns'] != []:
-                  for serviceName in ecsServicesToDelete['serviceArns']:
-                     ecsDeleteResponse = ecsClient.delete_service(
-                        cluster=EcsClusterName,
-                        service=serviceName,
-                        force=True)
+                logger.info('deleting bucket {}'.format(MythicalArtifactBucket))
+                bucket = s3.Bucket(MythicalArtifactBucket)
+                # Delete S3 bucket
+                for key in bucket.objects.all():
+                    key.delete()
+                bucket.delete()
             except Exception as e:
-               failure = True
-               tempResponse.append({EcsClusterName:str(e)})
-         responseData['Data'] = str(tempResponse)
-         if failure == False:
+                if "The specified bucket does not exist" in str(e):
+                    failure = False
+                else:
+                    failure = True
+                tempResponse.append({MythicalArtifactBucket: str(e)})
+
+            try:
+                # List ECS Services and delete services in a specific cluster
+                ecsServicesToDelete = ecsClient.list_services(
+                   cluster=EcsClusterName
+                )
+                if ecsServicesToDelete['serviceArns'] != []:
+                    for serviceName in ecsServicesToDelete['serviceArns']:
+                        ecsDeleteResponse = ecsClient.delete_service(
+                            cluster=EcsClusterName,
+                            service=serviceName,
+                            force=True)
+            except Exception as e:
+                failure = True
+                tempResponse.append({EcsClusterName: str(e)})
+            # failfast(event, context, SUCCESS, tempResponse, 'CustomResourcePhysicalID')
+
+        responseData['Data'] = str(tempResponse)
+        if failure == False:
             send(event, context, SUCCESS, responseData, 'CustomResourcePhysicalID')
-         else:
+        else:
+            send(event, context, FAILED, responseData, 'CustomResourcePhysicalID')   
+
+    if event['RequestType'] == 'Create':
+        # Setup the account to enable container insights by default
+        try:
+            responseData = {}
+            logger.info('setting account defaults for ECS')
+            ecsClient = boto3.client('ecs')
+            response = ecsClient.put_account_setting_default(name='containerInsights',value='enabled')
+            responseData['Data'] = str(response)
+            send(event, context, SUCCESS, responseData, 'CustomResourcePhysicalID')
+        except Exception as e:
+            responseData['Data'] = str(e)
             send(event, context, FAILED, responseData, 'CustomResourcePhysicalID')
 
-   if event['RequestType'] == 'Create':
-      # Setup the account to enable container insights by default
-      try:
-         responseData = {}
-         logger.debug('setting account defaults for ECS')
-         ecsClient = boto3.client('ecs')
-         response = ecsClient.put_account_setting_default(name='containerInsights',value='enabled')
-         responseData['Data'] = str(response)
-         send(event, context, SUCCESS, responseData, 'CustomResourcePhysicalID')
-      except Exception as e:
-         responseData['Data'] = str(e)
-         send(event, context, FAILED, responseData, 'CustomResourcePhysicalID')
-
-   if event['RequestType'] == 'Update':
-      try:
-         responseData = {}
-         send(event, context, SUCCESS, responseData, 'CustomResourcePhysicalID')
-      except Exception as e:
-         send(event, context, FAILED, responseData, 'CustomResourcePhysicalID')
+    if event['RequestType'] == 'Update':
+        try:
+            responseData = {}
+            send(event, context, SUCCESS, responseData, 'CustomResourcePhysicalID')
+        except Exception as e:
+            send(event, context, FAILED, responseData, 'CustomResourcePhysicalID')
 
 if __name__ == "__main__":
-	mockevent = {
-		'RequestType': 'Create',
+    mockevent = {
+		'RequestType': 'Delete',
 		'ServiceToken': 'arn:aws:lambda:us-east-2:428505257828:function:l1-CleanDeletionLambdaFunction-1I8XLIN9Q0RS4',
 		'ResponseURL': 'https://cloudformation-custom-resource-response-useast2.s3.us-east-2.amazonaws.com/arn%3Aaws%3Acloudformation%3Aus-east-2%3A428505257828%3Astack/l1/897739d0-227d-11ea-ba13-0ad7a569cb8c%7CCleanDeletionCustomResource%7Cb1587f31-65b4-4a29-8723-aef7d0f6d839?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20191219T163659Z&X-Amz-SignedHeaders=host&X-Amz-Expires=7200&X-Amz-Credential=AKIAVRFIPK6PMQZL3WHJ%2F20191219%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Signature=4feba47f210737c2155d1cb3147624f663d64926662cef9e088cf605b7175bb5',
 		'StackId': 'arn:aws:cloudformation:us-east-2:428505257828:stack/l1/897739d0-227d-11ea-ba13-0ad7a569cb8c',
@@ -167,5 +179,5 @@ if __name__ == "__main__":
 			'LikeServiceRepo': 'l1-likeserv-1dtsq9dl51m7z'
 		}
 	}
-	handler(event=mockevent,context={})
-	exit()
+
+    handler(event=mockevent,context={})
